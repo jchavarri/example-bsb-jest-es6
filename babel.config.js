3de697e38983837b353ac614fc5bc145645c2144
Javier Chávarri <javier.chavarri@gmail.com>
2019-05-02 22:28:23 +0200

    Remove Babel dep and use only es2015 modules plugin

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,7 @@
 module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          node: "current"
-        }
-      }
-    ]
-  ]
+  env: {
+    test: {
+      plugins: ["transform-es2015-modules-commonjs"]
+    }
+  }
 };
